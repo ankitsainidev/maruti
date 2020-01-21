06617be94e263bd47e1f33e3f10c0d06a480deb5
ankit <ankitsaini100205@gmail.com>
2020-01-22 02:23:48 +0530

    adding README

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="maruti",
+    version="0.0.1",
+    author="Ankit Saini",
+    author_email="ankitsaini100205@gmail.com",
+    description="Maruti Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ankitsainidev/maruti",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[]
+)
--- tests/test_deepfake.py
+++ tests/test_deepfake.py
@@ -1,4 +1,7 @@
 import unittest
+
+
 class DeepfakeTest(unittest.TestCase):
+
     def test_add(self):
-        self.assertEqual(3+2,5)
+        self.assertEqual(3+2, 5)
