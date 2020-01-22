9dba779a5105357de43a44d1c135558b9df42a19
ankit <ankitsaini100205@gmail.com>
2020-01-23 00:01:26 +0530

    version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="maruti",
-    version="0.0.5",
+    version="0.0.6",
     author="Ankit Saini",
     author_email="ankitsaini100205@gmail.com",
     description="Maruti Library",
