6fbf8f8c68f499bb978ac92546e9b3964ec7dd9a
ankit <ankitsaini100205@gmail.com>
2020-01-23 15:50:22 +0530

    removing manifest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="maruti",
-    version="0.1.0",
+    version="0.1.1",
     author="Ankit Saini",
     author_email="ankitsaini100205@gmail.com",
     description="Maruti Library",
