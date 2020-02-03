7f3dd3fd0fa4d6905487f04da5f0b31808ea145b
akanksha saini <akankshasaini03@gmail.com>
2020-02-03 19:16:56 +0530

    adding import stability

--- maruti/__init__.py
+++ maruti/__init__.py
@@ -2,7 +2,7 @@ from . import utils
 from . import sizes
 from . import vision
 from . import deepfake
-from . import deepfake
+from . import kaggle
 from .utils import *
 from .sizes import *
 from .deepfake import VideoDataset, Learner
