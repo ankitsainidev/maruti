585987e3ca839a677f29f37ae1e513138ff6fc89
akanksha saini <akankshasaini03@gmail.com>
2020-02-04 14:30:07 +0530

    stabilizing import

--- maruti/__init__.py
+++ maruti/__init__.py
@@ -3,6 +3,8 @@ from . import sizes
 from . import vision
 from . import deepfake
 from . import kaggle
+from . import torch
 from .utils import *
 from .sizes import *
-from .deepfake import VideoDataset, Learner
+from .deepfake import VideoDataset
+from .torch import Learner
--- maruti/deepfake/__init__.py
+++ maruti/deepfake/__init__.py
@@ -1,5 +1,3 @@
 from . import dataset
 from . import models
-from . import utils
 from .dataset import VideoDataset
-from .utils import Learner
