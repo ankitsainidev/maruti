c9bea8f2ec3b3eac38eaf7246d0819c3836b041a
ankit saini <ankitsaini100205@gmail.com>
2020-02-09 12:41:27 +0530

    fixing typo

--- maruti/imports/ml.py
+++ maruti/imports/ml.py
@@ -1,5 +1,5 @@
 from .general import *
-from general import __all__ as gen_all
+from .general import __all__ as gen_all
 import torch
 import torch.nn as nn
 import torchvision.transforms as torch_transforms
@@ -8,4 +8,5 @@ import maruti.torch as mtorch
 import maruti
 from torch.utils import data
 import torch.optim as optim
-__all__ = gen_all+['torch','nn','torch_transforms','torchvision','mtorch','maruti','data','optim']
+__all__ = gen_all + ['torch', 'nn', 'torch_transforms',
+                     'torchvision', 'mtorch', 'maruti', 'data', 'optim']
