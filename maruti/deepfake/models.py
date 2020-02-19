16433f3554ac6897557691c83a6ef13f6d14b873
ankit saini <ankitsaini100205@gmail.com>
2020-02-19 22:03:53 +0530

    model improvement

--- maruti/deepfake/models.py
+++ maruti/deepfake/models.py
@@ -3,39 +3,69 @@ import torchvision
 from ..torch.utils import freeze
 
 
-def resnext50(pretrained=False, feature=False, hidden_size=64):
-    """feature:int weather to return hidden_size sized feature vector or binary classification"""
+def resnext50(feature=False, pretrained=False):
     model = torchvision.models.resnext50_32x4d(pretrained)
-    model.fc = nn.Sequential(nn.Linear(2048, hidden_size),
-                             nn.BatchNorm1d(64), nn.Linear(64, 1))
     if feature:
-        model.fc[2] = nn.Identity()
-    freeze(model)
+        model.fc = nn.Identity()
+    else:
+        model.fc = nn.Linear(2048, 1)
     return model
 
 
-class MultiFrame(nn.Module):
-    """
-    (model
-    model
-    model     -----   linar batchnorm linear -> output
-    .
-    .
-    .)
-    total frames
-    """
+class ResLSTM(nn.Module):
 
-    def __init__(self, model, frames, feature_size=64):
+    def __init__(self, hidden_size=128):
         super().__init__()
-        self.feature = model
-        self.frames = frames
-        self.feature_size = feature_size
-        self.fc = nn.Sequential(nn.Linear(self.feature_size * self.frames, 16),
-                                nn.BatchNorm1d(16), nn.Linear(16, 1))
+        # resnext
+        self.feature_model = resnext50(True)
+
+        # lstm
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(2048, self.hidden_size, bidirectional=False)
+        self.classifier = nn.Linear(self.hidden_size, 1)
 
     def forward(self, x):
-        batch_size = x.shape[0]
-        assert x.shape[1:] == (self.frames, 3, 224, 224)
-        x = self.feature(x.view(-1, 3, 224, 224))
-        x = x.view(batch_size, -1)
-        return self.fc(x)
+        # indices
+        unsorted_indices = x.unsorted_indices
+
+        # prediction on all images from each batch
+        x_data = self.feature_model(x.data)
+
+        # converting again to PackedSequence
+        x = PackedSequence(x_data, x.batch_sizes)
+
+        # lstm
+        out, (h, c) = self.lstm(x)
+        batch_size = h.shape[1]
+
+        # treat each batch differently instaed of lstm layer
+        split_on_batch = h.permute(1, 0, 2)
+
+        # reshape to make each bach flat
+        combining_passes = split_on_batch.reshape(batch_size, -1)
+
+        # classify
+        val = self.classifier(combining_passes).squeeze(1)
+        return val[unsorted_indices]
+
+    def param(self, i=-1):
+        # all
+        if i == -1:
+            return self.parameters()
+
+        # grouped
+        if i == 0:
+            return itertools.chain(self.feature_model.conv1.parameters(),
+                                   self.feature_model.bn1.parameters(),
+                                   self.feature_model.layer1.parameters(),)
+        if i == 1:
+            return itertools.chain(self.feature_model.layer2.parameters(),
+                                   self.feature_model.layer3.parameters())
+        if i == 2:
+            return itertools.chain(self.feature_model.layer4.parameters(),
+                                   self.feature_model.fc.parameters())
+        if i == 3:
+            return itertools.chain(self.lstm.parameters(),
+                                   self.classifier.parameters())
+        else:
+            print('there are only 4 param groups -> 0,1,2,3')
