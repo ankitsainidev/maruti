ab5a292dd0d9785f107e085f78c3025468c09498
ankit saini <ankitsaini100205@gmail.com>
2020-02-23 22:52:59 +0530

    efficient face detection

--- maruti/vision/video.py
+++ maruti/vision/video.py
@@ -4,6 +4,7 @@ from .. import vision as mvis
 from facenet_pytorch import MTCNN
 import torch
 from PIL import Image
+from collections import defaultdict
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 
@@ -66,19 +67,11 @@ def bbox_from_det(det_list):
     return bbox
 
 
-def get_face_frames2(path, start, end, jumps=4, margin=30, mtcnn=None, size: "(h,w)" = (224, 224)):
-    cap = cv2.VideoCapture(path)
-    f_h, f_w = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(
-        cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    cap.release()
-    #
-    frame_idx = list(range(start, end))
-    detect_idx = list(range(start, end, jumps))
+def _face_from_frames(frame_idx, detect_idx, frames, f_h, f_w, margin=30, size=size, mtcnn=None):
+    start = frame_idx[0]
     n_h, n_w = f_h // 2, f_w // 2
     if mtcnn is None:
         mtcnn = MTCNN(select_largest=False, device=device,)
-
-    frames = list(get_frames_from_path(path, frame_idx))
     small_faces = [cv2.resize(frame, (n_w, n_h))
                    for i, frame in enumerate(frames) if i + start in detect_idx]
     det, conf = mtcnn.detect(small_faces)
@@ -86,7 +79,7 @@ def get_face_frames2(path, start, end, jumps=4, margin=30, mtcnn=None, size: "(h
     det_list = list(map(lambda x: x, det))
 
     for i, box in zip(detect_idx, det_list):
-        full_det_list[i] = box
+        full_det_list[i - start] = box
     bbox = bbox_from_det(full_det_list)
     working_pred = np.array([(f_h // 2) - 112, (f_w // 2) - 112,
                              (f_h // 2) + 112, (f_h // 2) + 112])
@@ -103,6 +96,74 @@ def get_face_frames2(path, start, end, jumps=4, margin=30, mtcnn=None, size: "(h
         faces.append(cropped_faces)
 
     return faces
+
+
+def non_overlapping_ranges(rngs):
+    all_idx = set()
+    for rng in rngs:
+        for i in range(rng[0], rng[1]):
+            all_idx.add(i)
+    min_i = min(all_idx)
+    max_i = max(all_idx)
+    non_overlapping_rngs = []
+    last = min_i
+    start = min_i
+    i = min_i + 1
+    print(all_idx)
+    while i < max_i + 1:
+        if i in all_idx:
+            last = i
+            i += 1
+            continue
+        else:
+            non_overlapping_rngs.append([start, last + 1])
+            while i not in all_idx:
+                i += 1
+            start = i
+            last = i
+    non_overlapping_rngs.append([start, last + 1])
+    return non_overlapping_rngs
+
+
+def get_face_frames2(path, frame_rngs, jumps=4, margin=30, mtcnn=None, size: "(h,w)" = (224, 224)):
+    # for height and width
+    cap = cv2.VideoCapture(path)
+    f_h, f_w = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(
+        cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    cap.release()
+    #
+    non_overlapping_rngs = non_overlapping_ranges(frame_rngs)
+    idx2face = defaultdict(lambda: None)
+    idx2frame = defaultdict(lambda: None)
+    if mtcnn is None:
+        mtcnn = MTCNN(select_largest=False, device=device,)
+
+    # getting video frames in one shot
+    all_frames_idx = []
+    for rng in non_overlapping_rngs:
+        all_frames_idx.extend(range(rng[0], rng[1]))
+    vid_frames = list(get_frames_from_path(path, all_frames_idx))
+    for i, frame in zip(all_frames_idx, vid_frames):
+        idx2frame[i] = frame
+
+    # detection
+    for frame_rng in non_overlapping_rngs:
+        start, end = frame_rng
+        frame_idx = list(range(start, end))
+        detect_idx = list(range(start, end, jumps))
+        frames = [idx2frame[i] for i in frame_idx]
+        faces = _face_from_frames(
+            frame_idx, detect_idx, frames, f_h, f_w, margin=margin, size=size, mtcnn=mtcnn)
+        for i, face in zip(frame_idx, faces):
+            idx2face[i] = face
+
+    # rearrangement
+    rng_faces = []
+    for rng in frame_rngs:
+        curr_rng_faces = []
+        for i in range(rng[0], rng[1]):
+            curr_rng_faces.append(idx2face[i])
+    return rng_faces
 
 
 def get_face_frames(path, frame_idx, margin=30, mtcnn=None, size: "(h,w)" = (224, 224),):
