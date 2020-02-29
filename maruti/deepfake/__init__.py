151571fda6cb8e79b95e5e32f24dc60e3f57d782
ankit saini <ankitsaini100205@gmail.com>
2020-03-01 00:09:43 +0530

    typo

--- maruti/deepfake/__init__.py
+++ maruti/deepfake/__init__.py
@@ -1,4 +1,4 @@
 from . import dataset
 from . import models
 from .dataset import VideoDataset, DeepfakeDataset
-from dataset import transform, group_transform
+from .dataset import transform, group_transform
