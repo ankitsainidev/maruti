8e282924a8f0f00fd4e468821c4dcd68a32d52de
akanksha saini <akankshasaini03@gmail.com>
2020-03-16 09:57:09 +0530

    updating setup

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
-    install_requires=['tqdm==4.40.2', 'opencv-python', 'torch-lr-finder']
+    install_requires=['tqdm==4.40.2', 'opencv-python', 'facenet_pytorch']
 )
